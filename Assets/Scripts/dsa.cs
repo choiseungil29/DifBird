443f55cb9af76b40c94df9f7be249f44076d5aa2
Choi SeungIl <choiseungil29@gmail.com>
2013-03-07 19:54:02 +0900

    settings unity horror project

---
+++ Assets/Scripts/dsa.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class dsa : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //asdasd
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
