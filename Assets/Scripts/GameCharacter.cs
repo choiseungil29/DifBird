67766c5d0621ccfd81635ee2529912b0c72da69e
Choi SeungIl <choiseungil29@gmail.com>
2013-03-12 15:51:10 +0900

    GameCharacter.cs -> code Optimization. ( add Movement, Rotation function)

--- Assets/Scripts/GameCharacter.cs
+++ Assets/Scripts/GameCharacter.cs
@@ -28,25 +28,34 @@ public class GameCharacter : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        Rotation();
+	}
+
+    void Movement()
+    {
         this.transform.Translate(Vector3.forward * Input.GetAxis("Forward") * Time.deltaTime * speed);
         this.transform.Translate(Vector3.left * Input.GetAxis("Left") * Time.deltaTime * speed);
         this.transform.Translate(Vector3.back * Input.GetAxis("Back") * Time.deltaTime * speed);
         this.transform.Translate(Vector3.right * Input.GetAxis("Right") * Time.deltaTime * speed);
         // Character Movement
+    }
 
-        Vector3 CharacterPos = transform.position;
+    void Rotation()
+    {
+        Vector3 CharacterPos = this.transform.position;
         camera.transform.position = new Vector3(CharacterPos.x, CharacterPos.y, CharacterPos.z - cameraDistance);
         camera.transform.LookAt(this.transform);
 
-        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f; // 좌, 우(x방향)으로 돌아감
-        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f; // 상, 하(y방향)으로 돌아감
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f; // 0.02f -> Mouse Sensibility
 
         y = Mathf.Clamp(y, -5.0f, 20.0f);
 
-        this.transform.rotation = Quaternion.Euler(y, x, 0); // x, y, z를 축으로 한 회전값 반환
+        this.transform.rotation = Quaternion.Euler(y, x, 0); // axis of x, y, z rotation value transform
 
         Vector3 characterAng = this.transform.rotation.eulerAngles;
         camera.transform.RotateAround(CharacterPos, camera.transform.TransformDirection(Vector3.up), characterAng.y); // Z축도 같이 내려간다. 이걸 수정
         camera.transform.RotateAround(CharacterPos, camera.transform.TransformDirection(Vector3.right), characterAng.x);
-	}
+    }
 }
