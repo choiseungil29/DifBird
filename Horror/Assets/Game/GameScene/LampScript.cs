b2641c3689950e84b26f542fd82593fb6ea712a2
unknown <pbes0707@naver.com>
2013-05-15 01:43:52 +0900

    Player Switch light

--- Horror/Assets/Game/GameScene/LampScript.cs
+++ Horror/Assets/Game/GameScene/LampScript.cs
@@ -5,23 +5,22 @@ public class LampScript : MonoBehaviour {
 
     public GameObject pivot;
     bool flag = true;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate ()
+    {
+        MoveLight();
+	}
+    void MoveLight()
     {
         Debug.Log("x : " + transform.localRotation.eulerAngles.x);
         if (this.transform.localRotation.eulerAngles.x > 290)
         {
             if (flag == false) flag = true;
             else flag = false;
-            Debug.Log("false"); 
         }
-
         if (flag == true) gameObject.transform.RotateAround(pivot.transform.position, this.transform.TransformDirection(Vector3.up), 10 * Time.deltaTime);
         else gameObject.transform.RotateAround(pivot.transform.position, this.transform.TransformDirection(Vector3.down), 10 * Time.deltaTime);
-	}
+    }
+    
 }
