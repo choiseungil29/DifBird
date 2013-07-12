e7776e22f5f8769826ac5b3bb14dd7e0a863f15b
unknown <pbes0707@naver.com>
2013-07-12 17:51:45 +0900

    SubEvent

---
+++ Horror/Assets/Game/AIScene/Scripts/suddenevent_move.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class suddenevent_move : MonoBehaviour {
+
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+	}
+    public static void event1()
+    {
+        Debug.Log("event1");
+    }
+    public static void event2()
+    {
+        Debug.Log("event2");
+    }
+    public static void gaa()
+    {
+        Debug.Log("gaa");
+    }
+}
